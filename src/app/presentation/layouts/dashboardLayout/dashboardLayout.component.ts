efae90b0ec443302210460a87725f98473e50417
melvinparra <135729962+melvinparra@users.noreply.github.com>
2024-09-18 22:04:27 -0400

    Dashboard added

--- src/app/presentation/layouts/dashboardLayout/dashboardLayout.component.ts
+++ src/app/presentation/layouts/dashboardLayout/dashboardLayout.component.ts
@@ -1,15 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SidebarMenuitemComponent } from "../../components/sidebarMenuitem/sidebarMenuitem.component";
+import {routes} from '../../../app.routes'
 
 @Component({
   selector: 'app-dashboard-layout',
   standalone: true,
   imports: [
     CommonModule,
-    RouterModule
-  ],
+    RouterModule,
+    DashboardLayoutComponent,
+    SidebarMenuitemComponent
+],
   templateUrl: './dashboardLayout.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DashboardLayoutComponent { }
+export class DashboardLayoutComponent { 
+
+
+  public routes = routes[0].children?.filter(route => route.data )
+}
